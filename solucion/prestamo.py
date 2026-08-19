23eeb6536f7e1cc40b91c3d39876f56548ff00dc
Abril Nataly Calatayud <abril.calatayud.contacto@gmail.com>
2026-08-19 14:51:34 -0300

    First commit. Creé los archivos y la estructura de la clase Prestamo

---
+++ solucion/prestamo.py
@@ -0,0 +1,14 @@
+class Prestamo:
+    def __init__(self, titulo, nombre_socio, dias_transcurridos):
+        self.titulo = titulo
+        self.nombre_socio = nombre_socio
+        self.dias_transcurridos = dias_transcurridos
+
+    def esta_vencido(self):
+        pass
+
+    def dias_de_retraso(self):
+        pass
+
+    def resumen(self):
+        pass
