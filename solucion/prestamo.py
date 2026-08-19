e0038d0a4efe67f983d5748d96175959b1ff18c2
Abril Nataly Calatayud <abril.calatayud.contacto@gmail.com>
2026-08-19 14:55:56 -0300

    Medidas para que de ValueError si los datos son inválidos.

--- solucion/prestamo.py
+++ solucion/prestamo.py
@@ -1,5 +1,12 @@
 class Prestamo:
     def __init__(self, titulo, nombre_socio, dias_transcurridos):
+        if not titulo.strip():
+            raise ValueError("El titulo no puede estar vacio")
+        if not nombre_socio.strip():
+            raise ValueError("El nombre del socio no puede estar vacio")
+        if dias_transcurridos < 0:
+            raise ValueError(f"Los dias transcurriods no pueden ser negativos: {dias_transcurridos}")
+        
         self.titulo = titulo
         self.nombre_socio = nombre_socio
         self.dias_transcurridos = dias_transcurridos
