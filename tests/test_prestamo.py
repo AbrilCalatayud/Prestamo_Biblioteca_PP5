ff9d6c97738020ffd3ddc2f516b47896c6be2445
Abril Nataly Calatayud <abril.calatayud.contacto@gmail.com>
2026-08-21 19:33:11 -0300

    Definí las funciones que voy a usar para el testeo

---
+++ tests/test_prestamo.py
@@ -0,0 +1,20 @@
+import pytest
+
+class TestPrestamo:
+    def test_titulo_vacio_lanza_value_error(self):
+        pass
+
+    def test_socio_vacio_lanza_value_error(self):
+        pass
+
+    def test_dias_transcurridos_negativo_lanza_value_error(self):
+        pass
+
+    def test_prestamo_en_termino(self):
+        pass
+
+    def test_prestamo_vencido(self):
+        pass
+
+    def test_prestamo_retraso_cero(self):
+        pass
