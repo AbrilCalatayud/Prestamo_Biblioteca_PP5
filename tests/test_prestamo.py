4bd6726c715909209c4f02915dd5126a228a3225
Abril Nataly Calatayud <abril.calatayud.contacto@gmail.com>
2026-08-21 19:40:50 -0300

    Creé los métodos que testean datos inválidos

--- tests/test_prestamo.py
+++ tests/test_prestamo.py
@@ -1,14 +1,18 @@
 import pytest
+from solucion.prestamo import Prestamo
 
 class TestPrestamo:
     def test_titulo_vacio_lanza_value_error(self):
-        pass
+        with pytest.raises(ValueError):
+            Prestamo("", "Pablo Aguirre", 4)
 
-    def test_socio_vacio_lanza_value_error(self):
-        pass
+    def test_nombre_socio_vacio_lanza_value_error(self):
+        with pytest.raises(ValueError):
+            Prestamo("El Principito", "", 4)
 
     def test_dias_transcurridos_negativo_lanza_value_error(self):
-        pass
+        with pytest.raises(ValueError):
+            Prestamo("El Principito", "Pablo Aguirre", -4)
 
     def test_prestamo_en_termino(self):
         pass
